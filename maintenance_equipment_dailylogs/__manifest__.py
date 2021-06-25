8a975fea0a77d7b78cf576ffa2ddd91f817c1caf
Justin Mangini <justin@diyautospec.com>
2021-06-25 11:04:51 -0600

    [maintenance_equipment_dailylogs] create module

---
+++ maintenance_equipment_dailylogs/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 ForgeFlow S.L. (https://forgeflow.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Maintenance Equipment Daily Logs",
+    "summary": "Start up and shutdown procedures and daily PM's",
+    "author": "ESG",
+    "website": "",
+    "category": "Equipments, Assets, Internal Hardware, Allocation Tracking",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["maintenance"],
+    "data": ["views/maintenance_equipment_views.xml","security/ir.model.access.csv"],
+    "demo": ["data/demo_maintenance_equipment_hierarchy.xml"],
+}
