868f6b2aae45049109c2021c12ae33a4d2e27b98
Justin Mangini <justin@diyautospec.com>
2021-06-25 11:04:13 -0600

    [maintenance_equipment_attachments] create module

---
+++ maintenance_equipment_attachments/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Maintenance Equipment Attachments",
+    "summary": """
+        Manage equipment attachments""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Justin Mangini",
+    "website": "",
+    "depends": ["hr_maintenance"],
+    "data": ["views/views.xml", "security/ir.model.access.csv"],
+    "demo": [],
+}
